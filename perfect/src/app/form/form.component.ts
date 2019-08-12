db95572399c52956057e4b2158eba02e77d00f95
SushmithaPatlolla <sushmithareddy42@gmail.com>
2019-08-12 18:40:35 -0400

    first commit

---
+++ perfect/src/app/form/form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl,FormBuilder, FormGroup, Validators} from '@angular/forms'
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
